b9a614e18aa9f2f3101836ede08de91b4ea3d5d1
Blake Wyatt <nabakin@protonmail.ch>
2020-12-13 18:12:42 -0500

    Add Day 1 Part 2

--- 2020/day-1/src/lib.rs
+++ 2020/day-1/src/lib.rs
@@ -3,7 +3,7 @@ use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::cmp::Ordering;
 
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 pub fn run(path: &str) {
     print!("Day 1 Part 1 running...");
@@ -26,6 +26,31 @@ fn part_1(path: &str) -> Option<u32> {
         }
 
         visited.insert(expense);
+    }
+
+    None
+}
+
+fn part_2(path: &str) -> Option<u32> {
+    let expenses = file_lines_to_u32s(Path::new(path));
+    let mut visited = HashMap::new();
+    let mut counter = 0;
+
+    for i in 0..expenses.len() {
+        let expense = expenses[i];
+        let matching_expense = 2020 - expense;
+
+        if visited.contains_key(&matching_expense) {
+            let value = visited.get(&matching_expense).unwrap();
+
+            return Some(expense * value * (2020 - expense - value));
+        }
+
+        for i in 0..counter {
+            visited.insert(expense + expenses[i], expense);
+        }
+
+        counter += 1;
     }
 
     None
@@ -77,5 +102,10 @@ mod tests {
     #[test]
     fn part_1_valid() {
         assert_eq!(1013211, part_1("input.txt").unwrap());
+    }
+
+    #[test]
+    fn part_2_valid() {
+        assert_eq!(13891280, part_2("input.txt").unwrap());
     }
 }
